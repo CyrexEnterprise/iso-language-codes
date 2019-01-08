d37a3950bd2d4b1f3a826378f3e1e2bbfc931b40
Diogo Simoes <diogo.miguel.simoes@gmail.com>
2019-01-08 14:54:38 +0000

    chore(lint): adding eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"extends": "standard",
+	"rules": {
+	  "comma-dangle": ["error", "always-multiline"],
+	  "no-multiple-empty-lines": "off",
+	  "no-var": "error",
+	  "no-use-before-define": "off",
+	  "require-await": "warn",
+	  "no-trailing-spaces": "off"
+	}
+  };
